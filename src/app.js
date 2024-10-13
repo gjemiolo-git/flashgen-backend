4559cd0bdbc3f7d42066a55c0ecbf69487a4c077
Gregory Jemiolo <70952108+gjemiolo-git@users.noreply.github.com>
2024-10-13 18:16:50 +0100

    Early mvp (#1)
    * Refactor code
    
    * Update README

--- src/app.js
+++ src/app.js
@@ -7,63 +7,33 @@ const app = express();
 const passport = require('passport');
 const cors = require('cors');
 require('./middleware/passport-middleware');
-//const session = require('express-session');
-// const RedisStore = require('connect-redis').default;
-// const { redisClient, getAsync, setAsync } = require('./services/redisService');
 
-const { sequelize, initDb } = require('./db');
-const { wrapAsync, wrapAsyncGen } = require('./utils/wrapAsync');
-const { errorHandler, ExpressError } = require('./middleware/errorHandler');
-const { SERVER_PORT, SESSION_SECRET, CLIENT_URL } = require('./constants/index');
+const { initDb } = require('./db');
+const { wrapAsyncGen } = require('./utils/wrapAsync');
+const { errorHandler } = require('./middleware/errorHandler');
+const { SERVER_PORT, CLIENT_URL } = require('./constants/index');
 const cookieParser = require('cookie-parser');
 
 // Import routes
-const testDb = require('./routes/testDb');
 const authRoutes = require('./routes/auth');
 const aiRoutes = require('./routes/ai');
 
 // Testers
 const { createDummyUser } = require('./utils/dbHelpers');
-//const { checkRedisConnection } = require('./utils/testers');
 
 // Express
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(cors({ origin: CLIENT_URL, credentials: true }));
-// Redis
-// const redisStore = new RedisStore({
-//     client: redisClient,
-//     prefix: "flasgen:",
-// })
-
-// app.use(session({
-//     store: redisStore,
-//     secret: SESSION_SECRET,
-//     resave: false,
-//     saveUninitialized: false,
-//     cookie: {
-//         secure: process.env.NODE_ENV === 'production',
-//         httpOnly: true,
-//         maxAge: 1000 * 60 * 60 * 24 // 1 day*
-//     }
-// }));
 
 // Initialise routes
-app.use('/', testDb)
 app.use('/api/auth', authRoutes);
 app.use('/api/ai', aiRoutes);
 
-
 app.use(errorHandler);
 
-
-
 app.listen(SERVER_PORT, wrapAsyncGen(async () => {
-    // await createDummyUser();
-    //await checkRedisConnection();
-    //await redisClient.connect().catch(console.error);
     await initDb();
     console.log(`Server on port ${SERVER_PORT}`);
 }));
